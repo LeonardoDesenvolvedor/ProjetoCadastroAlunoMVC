91ccaf5ddc74aa13c8c8d1d4e4f4c01ff4bd99d3
leona <leona@DESKTOP-Q5345HU>
2021-10-14 18:43:09 -0300

    Criacão da Cadastro com rota

---
+++ CadastroAluno/CadastroAluno/Controllers/CadastroController.cs
@@ -0,0 +1,38 @@
+﻿using CadastroAluno.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadastroAluno.Controllers
+{
+    public class CadastroController : Controller
+    {
+
+        private static IList<Cadastro> Cadastros = new List<Cadastro>()
+        {
+            new Cadastro()
+            {
+                CadastroId = 1,
+                Nome = "Leonardo"
+            },
+
+            new Cadastro()
+            {
+                CadastroId = 2,
+                Nome = "Paula"
+            }
+
+
+        };
+
+
+
+        // GET: Cadastro
+        public ActionResult Index()
+        {
+            return View(Cadastros);
+        }
+    }
+}
---
+++ CadastroAluno/CadastroAluno/Models/Cadastro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CadastroAluno.Models
+{
+    public class Cadastro
+    {
+        public long CadastroId { get; set; }
+        public string Nome { get; set; }
+    }
+}
